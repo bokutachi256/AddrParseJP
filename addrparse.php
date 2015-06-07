1ceefd2747827f63033dd750e4add269d8f981dc
bokutachi256 <util15@abox8.so-net.ne.jp>
2015-06-07 11:13:10 +0900

    町丁目・番地・号を変換する部分を関数化した

--- addrparse.php
+++ addrparse.php
@@ -1,34 +1,60 @@
 <?php
-mb_regex_encoding('UTF-8');
+	mb_regex_encoding('UTF-8');
 
-// サンプルの住所
-$txt=array(
-	'東京市麹町区市谷田町１３番地',
-	'東京市麹町区市谷田町13',
-	'東京市麹町区市谷田町十三番地',
-	'東京市麹町区市谷田町十三番',
-	'東京市麹町区市谷田町１丁目３番地',
-	'東京市麹町区市谷田町一丁目三番地',
-	'東京市麹町区市谷田町1-3番地',
-	'東京市麹町区市谷田町１ー３',
-	'東京市麹町区市谷田町１１１２ー３',
-	'東京市麹町区市谷田町１２３０ー３',
-	'東京市麹町区市谷田町五〇ー３',
-	'東京市麹町区市谷田町百五号',
-);
+	// サンプルの住所
+	$txt=array(
+		'東京市麹町区市谷田町１３番地',
+		'東京市麹町区市谷田町13',
+		'東京市麹町区市谷田町十三番地',
+		'東京市麹町区市谷田町十三番',
+		'東京市麹町区市谷田町１丁目３番地',
+		'東京市麹町区市谷田町一丁目三番地',
+		'東京市麹町区市谷田町1-3番地',
+		'東京市麹町区市谷田町１ー３',
+		'東京市麹町区市谷田町１１１２ー３',
+		'東京市麹町区市谷田町１２３０ー３',
+		'東京市麹町区市谷田町五〇ー３',
+		'東京市麹町区市谷田町百五号',
+	);
 
-// 漢数字->半角算用数字変換テーブル
-$sea1=array('一','二','三','四','五','六','七','八','九','〇');
-// 全角数字->半角算用数字変換テーブル
-$sea2=array('１','２','３','４','５','６','７','８','９','０');
-$repl1=array('1','2','3','4','5','6','7','8','9','0');
+	foreach($txt as $key=>$value){
+		echo "オリジナル：".$value."\n";
+		echo "変換後：".AddrConv($value)."\n";
+		echo "------------\n";
+	}
+	
 
-foreach($txt as $key=>$value) {
-	echo "オリジナル: ";
-	echo $value;
-	echo "\n";
-	$txt1=$value;
 
+	// 住所データベースへのアクセス
+
+	try{
+		$ad_db = new PDO('sqlite:ad_table.db');
+		$sql="select * from address_tab";
+		$res=$ad_db->query($sql);
+	}catch (PDOException $e){
+		print('Connection failed:'.$e->getMessage());
+		die();
+	}
+
+	try{
+		$db = new PDO('sqlite::memory:');
+	}catch (PDOException $e){
+		print('Connection failed:'.$e->getMessage());
+		die();
+	}
+
+	$db=null;
+
+function AddrConv($indata){
+
+	// 漢数字->半角算用数字変換テーブル
+	$sea1=array('一','二','三','四','五','六','七','八','九','〇');
+	// 全角数字->半角算用数字変換テーブル
+	$sea2=array('１','２','３','４','５','６','７','８','９','０');
+	$repl1=array('1','2','3','4','5','6','7','8','9','0');
+	
+	$txt1=$indata;
+	
 	//	漢数字・全角数字を半角算用数字に変換;
 	foreach($sea1 as $key=>$value){
 		$txt1=mb_ereg_replace($value, $repl1[$key], $txt1);
@@ -42,12 +68,11 @@ foreach($txt as $key=>$value) {
 	foreach($sea3 as $key=>$value){
 		$pattern="([^0-9])(".$value.")";
 		$txt1=mb_ereg_replace($pattern, '\11\2', $txt1);
-		
 	}
 	
 	// '1千2十'->1020のパターン
 	$pattern="([0-9])(千)([0-9]+)(十)";
-	$txt1=mb_ereg_replace($pattern, '\10\3\4', $txt1);	
+	$txt1=mb_ereg_replace($pattern, '\10\3\4', $txt1);
 	// '1百5'->105のパターン
 	$pattern="([0-9])(百)([0-9]+)([^十]|$)";
 	$txt1=mb_ereg_replace($pattern, '\10\3\4', $txt1);
@@ -74,31 +99,8 @@ foreach($txt as $key=>$value) {
 	$pattern="([0-9]+)([^0-9]+)";
 	$txt1 = mb_ereg_replace($pattern,'\1-',$txt1);
 	$txt1 = mb_ereg_replace('[^0-9]$','',$txt1);
-	echo "変換後: ";
-	echo $txt1;
-	echo "\n";		
 
-	echo "----------\n";
+	return($txt1);
 }
-
-// 住所データベースへのアクセス
-
-try{
-	$ad_db = new PDO('sqlite:ad_table.db');
-	$sql="select * from address_tab";
-	$res=$ad_db->query($sql);
-}catch (PDOException $e){
-	print('Connection failed:'.$e->getMessage());
-	die();
-}
-
-try{
-	$db = new PDO('sqlite::memory:');
-}catch (PDOException $e){
-	print('Connection failed:'.$e->getMessage());
-	die();
-}
-
-$db=null;
 
 ?>

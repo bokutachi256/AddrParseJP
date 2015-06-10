12d815a849fc3038b399c55e8f7c09084a129058
bokutachi256 <util15@abox8.so-net.ne.jp>
2015-06-10 18:29:07 +0900

    DPマッチングを行う関数を追加

--- libAddrParse.php
+++ libAddrParse.php
@@ -56,9 +56,103 @@ function AddrConv($indata){
 	// 町丁目・番・号のデリミタを'-'にする
 	$pattern="([0-9]+)([^0-9]+)";
 	$txt1 = mb_ereg_replace($pattern,'\1-',$txt1);
-	$txt1 = mb_ereg_replace('[^0-9]$','',$txt1);
-
-	return($txt1);
+//	$txt1 = mb_ereg_replace('[^0-9]$','',$txt1);
+	$txt1 = mb_ereg_replace('-$','',$txt1);
+	
+	return $txt1;
 }
 
+function dpmatching($instr_a, $instr_b, $penalty_mismatch, $penalty_gap){
+	/*
+	 DPマッチングを行う関数
+	入力文字列instr_aとinstr_bにおいて，一文字不一致ペナルティをpenalty_mismatch，
+	一文字ずれペナルティをpenalty_gapとした場合の不一致度distanceを返す関数
+	*/
+
+	// instr_a;	// 入力文字列Ａ
+	// instr_b;	// 入力文字列Ｂ
+
+	$length_a = mb_strlen($instr_a, 'UTF-8');	// 文字列Aの長さを取得
+	$length_b = mb_strlen($instr_b, 'UTF-8');	// 文字列Bの長さを取得
+
+	// 入力文字列$instr_aと$instr_bをそれぞれ1文字ずつ分解して$str_a[]と$str_b[]に格納する
+	for ($i=0; $i<$length_a; $i++){
+		$str_a[$i]=mb_substr($instr_a,$i, 1, 'UTF-8');
+	}
+
+	for ($i=0; $i<$length_b; $i++){
+		$str_b[$i]=mb_substr($instr_b,$i, 1, 'UTF-8');
+	}
+
+	$awazu_penalty = $penalty_mismatch;	// １文字不一致へのペナルティ
+	$zure_penalty = $penalty_gap;	// １文字ずれたことへのペナルティ
+
+	$distance = 0;	// ２つの文字列の不一致度
+
+	$missmatch[64][64]=0;	// 一致結果バッファ
+	$cost[64][64]=0;	// 各経路点の到達コスト
+	$from[64][64]=0;	// 最短経路はどこから来たか 0:斜め、1:ｉ増え,２：ｊ増え
+	$dtemp1=0;
+	$dtemp2=0;
+	$dtemp3=0;
+
+	// マッチング結果
+	$result_a[128]=0;
+	$result_b[128]=0;
+
+
+	// 総当たりで一致の確認
+	for($i = 0; $i < $length_a; $i++) {
+		for($j = 0; $j < $length_b; $j++) {
+			if($str_a[$i] == $str_b[$j]) {
+				$missmatch[$i][$j] = 0;
+			}
+			else {
+				$missmatch[$i][$j] = 1;
+			}
+		}
+	}
+
+	// コスト計算
+	$cost[0][0] = $missmatch[0][0] * $awazu_penalty;
+	$from[0][0] = 0;
+
+	// i側の縁
+	for($i = 1; $i < $length_a; $i++) {
+		$cost[$i][0] = $cost[$i-1][0] + $zure_penalty + $missmatch[$i][0] * $awazu_penalty;
+		$from[$i][0] = 1;
+	}
+	// ｊ側の縁
+	for($j = 1; $j < $length_b; $j++) {
+		$cost[0][$j] = $cost[0][$j-1] + $zure_penalty + $missmatch[0][$j] * $awazu_penalty;
+		$from[0][$j] = 2;
+	}
+
+	// 中間部
+	for($i = 1; $i < $length_a; $i++) {
+		for($j = 1; $j < $length_b; $j++) {
+			$dtemp1 = $cost[$i-1][$j-1] + $missmatch[$i][$j] * $awazu_penalty; //斜めで来た場合のコスト
+			$dtemp2 = $cost[$i-1][$j  ] + $missmatch[$i][$j] * $awazu_penalty + $zure_penalty; //i増えで来た場合のコスト
+			$dtemp3 = $cost[$i  ][$j-1] + $missmatch[$i][$j] * $awazu_penalty + $zure_penalty; //j増えで来た場合のコスト
+
+			if($dtemp1 <= $dtemp2 && $dtemp1 <= $dtemp3) {	// 縦横斜めのコストのうち，最小のものを採用
+				$cost[$i][$j] = $dtemp1;
+				$from[$i][$j] = 0;
+			}
+			else if($dtemp2 <= $dtemp3) {
+				$cost[$i][$j] = $dtemp2;
+				$from[$i][$j] = 1;
+			}
+			else {
+				$cost[$i][$j] = $dtemp3;
+				$from[$i][$j] = 2;
+			}
+		}
+	}
+
+	$distance = $cost[$length_a - 1][$length_b - 1];	// DPマッチングの不一致度
+
+	// 不一致度を出力して関数終了
+	return $distance;
+}
 ?>
